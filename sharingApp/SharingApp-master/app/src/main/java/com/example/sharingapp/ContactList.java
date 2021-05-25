caa6889a56f048ece33fb6c763705b5835126006
abdelrhman wahdan <abdelrhmanwahdan129@gmail.com>
2021-05-25 02:50:33 +0200

    Add files via upload

---
+++ sharingApp/SharingApp-master/app/src/main/java/com/example/sharingapp/ContactList.java
@@ -0,0 +1,124 @@
+package com.example.sharingapp;
+
+import android.content.Context;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class ContactList {
+private ArrayList<Contact> contacts;
+private final String FILENAME="contacts.sav" ;
+
+    public ContactList () {
+        this.contacts = new ArrayList< Contact >();
+    }
+    public void setContacts(ArrayList<Contact> contact_list){
+    this.contacts = contact_list;
+    }
+
+    public ArrayList<Contact> getContacts() {
+            return contacts;
+    }
+    public ArrayList<String> getAllUsernames() {
+        ArrayList<String> allUsernames = new ArrayList<>() ;
+        for (Contact c :contacts){
+            String username = c.getUsername();
+            allUsernames.add(username);
+        }
+        return allUsernames;
+    }
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+    public void deleteContact(Contact contact) {
+        contacts.remove(contact);
+    }
+    public Contact getContact(int index) {
+        return contacts.get(index);
+    }
+    public int getSize() {
+        return contacts.size();
+    }
+    public int getIndex(Contact contact) {
+        int pos = 0;
+        for (Contact i : contacts) {
+            if (contact.getId().equals(i.getId())) {
+                return pos;
+            }
+            pos = pos+1;
+        }
+        return -1;
+    }
+    public boolean hasContact(Contact contact) {
+        return contacts.contains(contact);
+    }
+    public Contact getContactByUsername(String username) {
+        Contact result = null;
+        for (Contact c : contacts) {
+            if (username.equals(c.getUsername())) {
+                result = c;
+                break;
+            }
+        }
+        return result;
+    }
+    public void loadContacts(Context context) {
+
+        try {
+            FileInputStream fis = context.openFileInput(this.FILENAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            Gson gson = new Gson();
+            Type listType = new TypeToken<ArrayList<Contact>>() {}.getType();
+            contacts = gson.fromJson(isr, listType); // temporary
+            fis.close();
+        } catch (FileNotFoundException e) {
+            contacts = new ArrayList<Contact>();
+        } catch (IOException e) {
+            contacts = new ArrayList<Contact>();
+        }
+    }
+
+    public void saveContacts(Context context) {
+        try {
+            FileOutputStream fos = context.openFileOutput(this.FILENAME, 0);
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
+            Gson gson = new Gson();
+            gson.toJson(contacts, osw);
+            osw.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isUsernameAvailable(String username) {
+
+        for (Contact c : contacts) {
+            if (username.equals(c.getUsername())) {
+
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+}
